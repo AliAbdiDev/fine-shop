ab898dd5ad357ef2e8e898293af47f48dfa9eb79
ali <aliabdidev@gmail.com>
2026-08-08 13:21:27 +0330

    Convert frontend submodule to regular directory

---
+++ frontend/next.config.ts
@@ -0,0 +1,21 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    formats: ['image/avif', 'image/webp'],
+  },
+
+  async headers() {
+    return [
+      {
+        source: '/(.*)', // all of routes
+        headers: [
+          { key: 'X-DNS-Prefetch-Control', value: 'on' }
+        ],
+      },
+    ]
+  },
+};
+
+export default nextConfig;
